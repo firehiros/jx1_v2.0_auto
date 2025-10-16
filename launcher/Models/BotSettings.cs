cb4d0f1dd58c94327dd3696008dbff5318f182ce
Kieu, QuocChien | ECMPD <ts-quocchien.kieu@rakuten.com>
2025-10-16 17:22:44 +0900

    update

--- launcher/Models/BotSettings.cs
+++ launcher/Models/BotSettings.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace JX1Launcher.Models
 {
--- launcher/Services/InjectorService.cs
+++ launcher/Services/InjectorService.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace JX1Launcher.Services
 {
